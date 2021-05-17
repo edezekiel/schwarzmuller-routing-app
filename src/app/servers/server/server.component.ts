d37cd7c9a0e55e4dc021dccf3fb23ed57818762b
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-05-17 12:01:08 -0500

    Load server by id using route params

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -10,10 +11,17 @@ import { ServersService } from '../servers.service';
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    this.server = this.serversService.getServer(parseInt(this.route.snapshot.params.id));
+
+    this.route.params.subscribe(p => {
+      this.server = this.serversService.getServer(parseInt(p.id));
+    });
   }
 
 }
