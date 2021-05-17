2393c1afac829cd2193c4909c0a4a4ec6f9b7db2
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2021-05-17 11:37:11 -0500

    Unsubscribe on User Component onDestroy

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+
+  subscription: Subscription = new Subscription();
 
   constructor(
     private router: ActivatedRoute
@@ -19,11 +22,14 @@ export class UserComponent implements OnInit {
       name: this.router.snapshot.params['name']
     };
 
-    this.router.params.subscribe(p => this.user = {
+    this.subscription.add(this.router.params.subscribe(p => this.user = {
       id: p.id,
       name: p.name
-    });
+    }));
+  }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
